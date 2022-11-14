500875e84a618a871e3b6f00830d6969207033ab
bini <ldabinn@gmail.com>
2022-11-15 02:02:22 +0900

    storeList 수정_!차

--- src/component/StoreList.jsx
+++ src/component/StoreList.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Flex, Text } from '@chakra-ui/react'
 import { css } from '@emotion/react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
-function StoreList({ placesList, placeElem }) {
+function StoreList({ placesList, placeElem, mustgo }) {
 
-    // useEffect(() => {
-    //     randomStore()
-    // }, [])
+    const [RecommendList, setRecommendList] = useState([])
+    useEffect(() => {
+        recommendPlace()
+    }, [])
 
-    // const randomStore= () => {
-
-    // }
+    const recommendPlace = async () => {
+        const request = await axios.get("http://localhost:3000/store/mustgo")
+        setRecommendList(request.data)
+    }
     const place = css`
         object-fit: contain;
         margin: 5px 5px 0 0;
@@ -40,38 +44,60 @@ function StoreList({ placesList, placeElem }) {
             `
 
 
-
     return (
-        <>
-            <Text fontSize={20}>추천 장소</Text>
+        mustgo ?
+            (
+                <>
+                    <Text fontSize={20}>true일때_검색 List 출력</Text>
 
-            <span className="arrow"
-                onClick={() => placeElem.current.scrollLeft -= window.innerWidth - 80}
-            >{'<'}</span>
+                    <span className="arrow"
+                        onClick={() => placeElem.current.scrollLeft -= window.innerWidth - 80}
+                    >{'<'}</span>
 
-            <Flex ref={placeElem} pt={5} mb={20} style={{ overflow: "hidden" }}>
-                {
-                    placesList.map((data) => (
-                        <div key={data.id} css={place}>
-                            <img src="https://image.freepik.com/free-photo/spaghetti-with-carbonara-sauce_1216-324.jpg" alt="markimg" />                            <div css={info}>
-                                <span css={markerbg}></span>
-                                <div>
-                                    <h5>{data.place_name}</h5>
-                                    <p>{data.road_address_name}</p>
-                                    <p>{data.phone}</p>
+                    <Flex ref={placeElem} pt={5} mb={20} style={{ overflow: "hidden" }}>
+                        {
+                            placesList.map((data) => (
+                                <div key={data.id} css={place}>
+                                    <img src="https://image.freepik.com/free-photo/spaghetti-with-carbonara-sauce_1216-324.jpg" alt="markimg" />                            <div css={info}>
+                                        <span css={markerbg}></span>
+                                        <div>
+                                            <h5>{data.place_name}</h5>
+                                            <p>{data.road_address_name}</p>
+                                            <p>{data.phone}</p>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                    ))
-                }
-            </Flex >
-            <span className="arrow"
-                onClick={() => placeElem.current.scrollLeft += window.innerWidth - 80}
-            >{'>'}</span>
+                            ))
+                        }
+                    </Flex >
+                    <span className="arrow"
+                        onClick={() => placeElem.current.scrollLeft += window.innerWidth - 80}
+                    >{'>'}</span>
+                </>
+            )
+            :
+            (
+                <>
+                    <Text fontSize={20}>false일때 default 화면</Text>
+                    <Flex ref={placeElem} pt={5} mb={20} style={{ overflow: "hidden" }}>
+                        {
+                            RecommendList.map((data) => (
+                                <div key={data.x} css={place}>
+                                    <img src={data.place_url} alt="markimg" />
+                                    <div css={info}>
+                                        <div>
+                                            <h5>{data.place_name}</h5>
+                                            <p>{data.road_address_name}</p>
+                                            <p>{data.phone}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </Flex>
+                </>
+            )
 
-
-
-        </>
     )
 }
 
